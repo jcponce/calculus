f060469ddce13a54ad50dc52581737bd8c4e5d6f
Juan Carlos Ponce Campuzano <37394697+jcponce@users.noreply.github.com>
2019-08-21 19:30:43 +1000

    Tag

--- velfields/RabinovichFabrikant/rabinovichfabrikant.js
+++ velfields/RabinovichFabrikant/rabinovichfabrikant.js
@@ -44,7 +44,7 @@ Randomize: randomCurve,
 
 
 function backAttractors () {
-    window.location.href = "https://jcponce.github.io/strange-attractors";
+    window.location.href = "https://jcponce.github.io/strange-attractors/#rabinovich";
 }
 
 
