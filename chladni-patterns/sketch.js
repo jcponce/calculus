eb67ecda377778b94a64713178de1ae73dfe10b2
Juan Carlos Ponce Campuzano <37394697+jcponce@users.noreply.github.com>
2022-06-11 10:25:51 +1000

    refactored

--- chladni-patterns/sketch.js
+++ chladni-patterns/sketch.js
@@ -35,13 +35,13 @@ let parDef = {
   bckgR: 0,
   bckgG: 0,
   bckgB: 0,
-  color: [ 0, 128, 255, 0.3 ],
+  color: [0, 128, 255, 0.3],
   opt: 100,
   red: 255,
   green: 255,
   blue: 255,
   hsbMod: false,
-  Save: function() {
+  Save: function () {
     save('chladni-pattern.jpg');
   },
 };
@@ -55,35 +55,37 @@ function setup() {
   pixelDensity(1);
   smooth();
   //frameRate(60);
-    
+
   //Gui controls
-  let gui = new dat.GUI( { width: 290 } );
+  let gui = new dat.GUI({
+    width: 290
+  });
   gui.add(parDef, 'play').name('Animation');
   gui.add(parDef, 'nParticles', 0, 2500, 1).name('Particles').listen(); // Number of particles on animation
   gui.add(parDef, 'frq', 0.03, 0.6, 0.001).name('Frequency').listen();
   //gui.add(parDef, 'elasticity', 0.001, 2, 0.001).name('Elasticity').listen();
-  gui.add(parDef, 'canvasSize', ['Square', 'Landscape', 'Full-Screen'] ).name("Size: ").onChange(screenSize);
-    
+  gui.add(parDef, 'canvasSize', ['Square', 'Landscape', 'Full-Screen']).name("Size: ").onChange(screenSize);
+
   let ocsGUI = gui.addFolder('Sources');
-    ocsGUI.add(parDef, 'showOcs').name('Show');
-    ocsGUI.add(parDef, 'nPoints', 1, 10, 1).name('n =').listen();
-    ocsGUI.add(this, 'resetPoints').name('Set position');
-    
+  ocsGUI.add(parDef, 'showOcs').name('Show');
+  ocsGUI.add(parDef, 'nPoints', 1, 10, 1).name('n =').listen();
+  ocsGUI.add(this, 'resetPoints').name('Set position');
+
   let pointsGUI = gui.addFolder('Particles settings');
-    pointsGUI.add(parDef, 'hsbMod').name('HSB');
-    pointsGUI.add(parDef, 'red', 0, 255, 1).name('Red/Hue').listen();
-    pointsGUI.add(parDef, 'green', 0, 255, 1).name('Green/Sat').listen();
-    pointsGUI.add(parDef, 'blue', 0, 255, 1).name('Blue/Brt').listen();
-    
-    //pointsGUI.add(parDef, 'color').name('Background').listen();
-    //let backGUI = gui.addFolder('Background settings');
-    //backGUI.add(parDef, 'bckgR', 0, 255, 1).name('Red').listen();
-    //backGUI.add(parDef, 'bckgG', 0, 255, 1).name('Green').listen();
-    //backGUI.add(parDef, 'bckgB', 0, 255, 1).name('Blue').listen();
-    pointsGUI.add(parDef, 'opt', 5, 100, 1).name('Trace').listen();
-    
+  pointsGUI.add(parDef, 'hsbMod').name('HSB');
+  pointsGUI.add(parDef, 'red', 0, 255, 1).name('Red/Hue').listen();
+  pointsGUI.add(parDef, 'green', 0, 255, 1).name('Green/Sat').listen();
+  pointsGUI.add(parDef, 'blue', 0, 255, 1).name('Blue/Brt').listen();
+
+  //pointsGUI.add(parDef, 'color').name('Background').listen();
+  //let backGUI = gui.addFolder('Background settings');
+  //backGUI.add(parDef, 'bckgR', 0, 255, 1).name('Red').listen();
+  //backGUI.add(parDef, 'bckgG', 0, 255, 1).name('Green').listen();
+  //backGUI.add(parDef, 'bckgB', 0, 255, 1).name('Blue').listen();
+  pointsGUI.add(parDef, 'opt', 5, 100, 1).name('Trace').listen();
+
   gui.add(parDef, 'Save').name('Save (jpg)');
-  
+
   gui.add(this, 'sourceCode').name('Source Code');
 
   gui.close();
@@ -97,33 +99,33 @@ function setup() {
   // Set Chladni particles to animate
   chl = new Chladni(N, ptsD);
   chl.init();
-    
+
   // Reset my dragable points to specific positions
   resetPoints();
-    
+
 } // setup()
 
 function draw() {
 
   background(parDef.bckgR, parDef.bckgG, parDef.bckgB, parDef.opt);
   cursor(ARROW)
-  time = millis()/1000;
-  
-  if(parDef.hsbMod===true){
-      colorMode(HSB, 255, 255, 255, 200);
+  time = millis() / 1000;
+
+  if (parDef.hsbMod === true) {
+    colorMode(HSB, 255, 255, 255, 200);
   } else colorMode(RGB, 255, 255, 255);
-    
-  chl.run();//Chladni is updated
+
+  chl.run(); //Chladni is updated
   chl.F = parDef.frq;
   chl.N = parDef.nParticles;
-  
+
   //Draggable objects
-  if(parDef.showOcs===true){
-      for (let drag of ptsD) {
-          drag.update();
-          drag.over();
-          drag.show();
-      }
+  if (parDef.showOcs === true) {
+    for (let drag of ptsD) {
+      drag.update();
+      drag.over();
+      drag.show();
+    }
   }
 
   //animationon/off
@@ -159,8 +161,8 @@ function draw() {
     text("Drag points around!", width / 2, 70);
     //text("Change values with controls", width / 2, 110)
   }
-    
-  if(time > 10 * 60){// 10 minutes max runnig time, so it does not crash. Suggested by Alex :)
+
+  if (time > 10 * 60) { // 10 minutes max runnig time, so it does not crash. Suggested by Alex :)
     noStroke()
     fill(70);
     rectMode(CENTER);
@@ -172,8 +174,8 @@ function draw() {
     text("Please, refresh your browser!", width / 2, 70);
     noLoop();
   }
-    
-    //chl.V = parDef.elasticy;
+
+  //chl.V = parDef.elasticy;
 
 } // draw()
 
@@ -193,51 +195,51 @@ function backHome() {
   window.location.href = "https://jcponce.github.io/#sketches";
 }
 
-function refreshPage(){
-    window.location.reload();
-} 
+function refreshPage() {
+  window.location.reload();
+}
 
 function screenSize() {
-    if (parDef.canvasSize == 'Square') {
-        resizeCanvas(500, 500);
-    } else if (parDef.canvasSize == 'Landscape') {
-        resizeCanvas(700, 500);
-    } else if (parDef.canvasSize == 'Full-Screen') {
-        resizeCanvas(windowWidth, windowHeight);
-    }
-    resetPoints();
+  if (parDef.canvasSize == 'Square') {
+    resizeCanvas(500, 500);
+  } else if (parDef.canvasSize == 'Landscape') {
+    resizeCanvas(700, 500);
+  } else if (parDef.canvasSize == 'Full-Screen') {
+    resizeCanvas(windowWidth, windowHeight);
+  }
+  resetPoints();
 }
 
 
 function resetPoints() {
-    let nx, ny;
-    let rd = 2.5;
-    for (let i = 0; i < ptsD.length; i++) {
-        if(parDef.nPoints===1){
-            nx = 0;
-            ny = 0;
-        }else if(parDef.nPoints===4){
-            nx = map(rd * cos(2 * PI * i / ptsD.length)- rd * sin(2 * PI * i / ptsD.length), 0, 2 * PI, 0, width);
-            ny = map(rd * cos(2 * PI * i / ptsD.length)+ rd * sin(2 * PI * i / ptsD.length), 0, 2 * PI, 0, height);
-        } else{
-            nx = map(rd * cos(2 * PI * i / ptsD.length), 0, 2 * PI, 0, width);
-            ny = map(rd * sin(2 * PI * i / ptsD.length), 0, 2 * PI, 0, height);
-        }
-        
-        ptsD[i].x = nx + width / 2;
-        ptsD[i].y = ny + height / 2;
+  let nx, ny;
+  let rd = 2.5;
+  for (let i = 0; i < ptsD.length; i++) {
+    if (parDef.nPoints === 1) {
+      nx = 0;
+      ny = 0;
+    } else if (parDef.nPoints === 4) {
+      nx = map(rd * cos(2 * PI * i / ptsD.length) - rd * sin(2 * PI * i / ptsD.length), 0, 2 * PI, 0, width);
+      ny = map(rd * cos(2 * PI * i / ptsD.length) + rd * sin(2 * PI * i / ptsD.length), 0, 2 * PI, 0, height);
+    } else {
+      nx = map(rd * cos(2 * PI * i / ptsD.length), 0, 2 * PI, 0, width);
+      ny = map(rd * sin(2 * PI * i / ptsD.length), 0, 2 * PI, 0, height);
     }
-    /*
-     ptsD[0].x = width / 2 - dif;
-     ptsD[0].y = height / 2 - dif;
-     ptsD[1].x = width / 2 + dif;
-     ptsD[1].y = height / 2 - dif;
-     ptsD[2].x = width / 2 - dif;
-     ptsD[2].y = height / 2 + dif;
-     ptsD[3].x = width / 2 + dif;
-     ptsD[3].y = height / 2 + dif;
-     */
-    
+
+    ptsD[i].x = nx + width / 2;
+    ptsD[i].y = ny + height / 2;
+  }
+  /*
+   ptsD[0].x = width / 2 - dif;
+   ptsD[0].y = height / 2 - dif;
+   ptsD[1].x = width / 2 + dif;
+   ptsD[1].y = height / 2 - dif;
+   ptsD[2].x = width / 2 - dif;
+   ptsD[2].y = height / 2 + dif;
+   ptsD[3].x = width / 2 + dif;
+   ptsD[3].y = height / 2 + dif;
+   */
+
 }
 
 let textIni = true;
@@ -253,7 +255,7 @@ function mousePressed() {
 
   for (let drag of ptsD) {
     drag.pressed();
-      //cursor('grab');
+    //cursor('grab');
   }
   textIni = false;
 
@@ -263,24 +265,22 @@ function mouseReleased() {
   for (let drag of ptsD) {
     // Quit dragging
     drag.released();
-      //cursor(ARROW);
+    //cursor(ARROW);
   }
 }
 
 function touchStarted() {
-    for (let drag of ptsD) {
-        drag.pressed();
-        cursor('grab');
-    }
-    textIni = false;
+  for (let drag of ptsD) {
+    drag.pressed();
+    cursor('grab');
+  }
+  textIni = false;
 }
 
 function touchEnded() {
-    for (let drag of ptsD) {
-        // Quit dragging
-        drag.released();
-        //cursor(ARROW);
-    }
+  for (let drag of ptsD) {
+    // Quit dragging
+    drag.released();
+    //cursor(ARROW);
+  }
 }
-
-
