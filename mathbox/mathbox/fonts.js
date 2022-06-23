8f2e47ab17bd36a5e882e1a075c2904d38c79fc5
Juan Carlos Ponce Campuzano <37394697+jcponce@users.noreply.github.com>
2022-06-23 19:30:48 +1000

    new files

---
+++ mathbox/mathbox/fonts.js
@@ -0,0 +1,2 @@
+let origin = location.origin.match(/acko\.net/) ? location.origin : 'https://acko.net';
+document.write('<link href="' + origin + '/fonts/KlavikaWeb/klavika-web.css" rel="stylesheet" type="text/css">');
